0544c9391fcff610a9309d3b6799f7d88b037722
piladd-design <piladd@yahoo.de>
2026-05-22 13:59:35 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'FORMULENS LAB',
+  description: 'Clinical cosmetic intelligence for skincare formulas.'
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
